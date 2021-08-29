e4b4c888888faa2846760f4e529fd4c8523b0b8e
amcmullen9291 <amcmullen9291@gmail.com>
2021-08-29 10:08:52 -0400

    starting Finish Listings

--- src/Constants/FinishesList.js
+++ src/Constants/FinishesList.js
@@ -10,16 +10,27 @@ function FinishesList(){
   const dispatch = useDispatch();
 
 let [finishesList, setFinishesList ] = useState([
-    {id: "1", nameOf: "a", material: "", premium_series: "", estate_series: "", elegant_bath: "", aspire: "", select_series: "", home_concepts: "", wellborn_closets: "", material: ""  },
-    {id: "2", nameOf: "s", material: "", premium_series: "", estate_series: "", elegant_bath: "", aspire: "", select_series: "", home_concepts: "", wellborn_closets: "", material: ""  },
-    {id: "3", nameOf: "d", material: "", premium_series: "", estate_series: "", elegant_bath: "", aspire: "", select_series: "", home_concepts: "", wellborn_closets: "", material: ""  },
-    {id: "4", nameOf: "r", material: "", premium_series: "", estate_series: "", elegant_bath: "", aspire: "", select_series: "", home_concepts: "", wellborn_closets: "", material: ""  },
-    {id: "5", nameOf: "g", material: "", premium_series: "", estate_series: "", elegant_bath: "", aspire: "", select_series: "", home_concepts: "", wellborn_closets: "", material: ""  },
-    {id: "6", nameOf: "i", material: "", premium_series: "", estate_series: "", elegant_bath: "", aspire: "", select_series: "", home_concepts: "", wellborn_closets: "", material: ""  },
-    {id: "7", nameOf: "p", material: "", premium_series: "", estate_series: "", elegant_bath: "", aspire: "", select_series: "", home_concepts: "", wellborn_closets: "", material: ""  },
-    {id: "8", nameOf: "w", material: "", premium_series: "", estate_series: "", elegant_bath: "", aspire: "", select_series: "", home_concepts: "", wellborn_closets: "", material: ""  },
-    {id: "9", nameOf: "qq", material: "", premium_series: "", estate_series: "", elegant_bath: "", aspire: "", select_series: "", home_concepts: "", wellborn_closets: "", material: ""  },
-    {id: "10", nameOf: "b", material: "", premium_series: "", estate_series: "", elegant_bath: "", aspire: "", select_series: "", home_concepts: "", wellborn_closets: "", material: ""  },
+    {id: "1", nameOf: "Autumn Spice", material: "Cherry", premium_series: "X", estate_series: "X", elegant_bath: "X", aspire: "X", select_series: "", home_concepts: "", wellborn_closets: "" },
+    {id: "2", nameOf: "Blush", material: "Cherry", premium_series: "X", estate_series: "X", elegant_bath: "X", aspire: "X", select_series: "X", home_concepts: "", wellborn_closets: "" },
+    {id: "3", nameOf: "Caramel", material: "Cherry", premium_series: "X", estate_series: "X", elegant_bath: "X", aspire: "X", select_series: "X", home_concepts: "", wellborn_closets: "" },
+    {id: "4", nameOf: "Dark", material: "Cherry", premium_series: "X", estate_series: "X", elegant_bath: "X", aspire: "X", select_series: "X", home_concepts: "", wellborn_closets: "" },
+    {id: "5", nameOf: "Drift", material: "Cherry", premium_series: "X", estate_series: "X", elegant_bath: "X", aspire: "X", select_series: "X", home_concepts: "", wellborn_closets: "" },
+    {id: "6", nameOf: "Espresso", material: "Cherry", premium_series: "X", estate_series: "X", elegant_bath: "X", aspire: "X", select_series: "X", home_concepts: "", wellborn_closets: "" },
+    {id: "7", nameOf: "Gauntlet", material: "Cherry", premium_series: "X", estate_series: "X", elegant_bath: "X", aspire: "X", select_series: "", home_concepts: "", wellborn_closets: "" },
+    {id: "8", nameOf: "Ginger", material: "Cherry", premium_series: "X", estate_series: "X", elegant_bath: "X", aspire: "X", select_series: "", home_concepts: "", wellborn_closets: "" },
+    {id: "9", nameOf: "Light", material: "Cherry", premium_series: "X", estate_series: "X", elegant_bath: "X", aspire: "X", select_series: "X", home_concepts: "", wellborn_closets: "" },
+    {id: "10", nameOf: "", material: "", premium_series: "", estate_series: "", elegant_bath: "", aspire: "", select_series: "", home_concepts: "", wellborn_closets: "" },
+    {id: "11", nameOf: "", material: "", premium_series: "", estate_series: "", elegant_bath: "", aspire: "", select_series: "", home_concepts: "", wellborn_closets: "" },
+    {id: "12", nameOf: "", material: "", premium_series: "", estate_series: "", elegant_bath: "", aspire: "", select_series: "", home_concepts: "", wellborn_closets: "" },
+    {id: "13", nameOf: "", material: "", premium_series: "", estate_series: "", elegant_bath: "", aspire: "", select_series: "", home_concepts: "", wellborn_closets: "" },
+    {id: "14", nameOf: "", material: "", premium_series: "", estate_series: "", elegant_bath: "", aspire: "", select_series: "", home_concepts: "", wellborn_closets: "" },
+    {id: "15", nameOf: "", material: "", premium_series: "", estate_series: "", elegant_bath: "", aspire: "", select_series: "", home_concepts: "", wellborn_closets: "" },
+    {id: "16", nameOf: "", material: "", premium_series: "", estate_series: "", elegant_bath: "", aspire: "", select_series: "", home_concepts: "", wellborn_closets: "" },
+    {id: "17", nameOf: "", material: "", premium_series: "", estate_series: "", elegant_bath: "", aspire: "", select_series: "", home_concepts: "", wellborn_closets: "" },
+    {id: "18", nameOf: "", material: "", premium_series: "", estate_series: "", elegant_bath: "", aspire: "", select_series: "", home_concepts: "", wellborn_closets: "" },
+    {id: "19", nameOf: "", material: "", premium_series: "", estate_series: "", elegant_bath: "", aspire: "", select_series: "", home_concepts: "", wellborn_closets: "" },
+    {id: "20", nameOf: "", material: "", premium_series: "", estate_series: "", elegant_bath: "", aspire: "", select_series: "", home_concepts: "", wellborn_closets: "" },
+    {id: "21", nameOf: "", material: "", premium_series: "", estate_series: "", elegant_bath: "", aspire: "", select_series: "", home_concepts: "", wellborn_closets: "" },
 
 ]);
 
